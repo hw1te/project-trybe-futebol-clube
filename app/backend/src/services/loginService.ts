780ad41a2d6c0e86bb9deebe8f45bb96b4579c0d
Gustavo Pospi <gutxander@gmail.com>
2022-08-31 16:32:09 -0300

    feat: requisito 2 e 4 feitos.

---
+++ app/backend/src/services/loginService.ts
@@ -0,0 +1,5 @@
+import User from '../database/models/users';
+
+export default class loginService {
+
+}
---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,36 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import User from '../database/models/users';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Testa o acesso ao endpoint /login.', () => {
+  it('Testa resposta da rota', async () => {
+     sinon.stub(User, "findOne").resolves(null);
+
+     const response = await chai.request(app).get('/login')
+     
+     expect(response.status).to.equal(201);
+
+     sinon.restore();
+  })
+
+  it('Retorna os usuários', async () => {
+    sinon.stub(User, "findOne").resolves(null);
+
+    const response = await chai.request(app).get('/login')
+
+
+    expect(response.body).to.be.deep.equal(null);
+
+    sinon.restore();
+  })
+});
