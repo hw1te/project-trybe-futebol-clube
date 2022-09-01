4bf2b32abecb1812af040aab1eb0abb1ea27d146
Gustavo Pospi <gutxander@gmail.com>
2022-09-01 15:26:15 -0300

    feat: requisitos 9 e 11 feitos.

--- app/backend/src/services/loginService.ts
+++ app/backend/src/services/loginService.ts
@@ -5,10 +5,15 @@ import userPayload from '../interfaces/interfacePayload';
 
 export default class LoginService {
   public login = async (payload: userPayload) => {
+    if (!payload.email || !payload.password) {
+      return { code: 400, data: { message: 'All fields must be filled' } };
+    }
     const user = await User.findOne({
       where: { email: payload.email },
     });
-    if (!user) { return { code: 400, data: { message: 'All fields must be filled' } }; }
+    if (user === null) {
+      return { code: 401, data: { message: 'Incorrect email or password' } };
+    }
     const validPassword = bcrypt.compareSync(payload.password, user.password);
     const token = jwt.sign({ email: user.email, id: user.id }, process.env.JWT_SECRET as string);
     if (validPassword) { return { code: 200, data: { token } }; }
